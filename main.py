fec97a5a5a17c1c37f9562900bfdff111a605204
yuu <crosscore.g@gmail.com>
2024-12-15 20:42:36 +0900

    Update: main.py for Railway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.app import app
+import os
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
